9a70096b30fd059f585c217e1daf7e417c3b0087
tstewart <tstewart@klaudhaus.com>
2019-11-27 10:10:46 +1100

    v 0.1.6 Fix paths in examples bin

--- examples/bin.js
+++ examples/bin.js
@@ -1,10 +1,18 @@
 #!/usr/bin/env node
 
+/**
+ * This script is linked from `bin` in the `lit-up` package.json
+ * to enable running `lit-up-examples` in any project that uses `lit-up`
+ *
+ * To see how to launch es-dev-server directly from shell or as an npm script,
+ * which will probably be a better fit for developing your app,
+ * look at the `examples` script in package.json for `lit-up` module
+ */
+
 const { createConfig, startServer } = require("es-dev-server")
 
 const config = createConfig({
-  rootDir: "..",
-  open: "examples/",
+  open: "node_modules/@klaudhaus/lit-up/examples/",
   nodeResolve: true
 })
 
