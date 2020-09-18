e95526ccd9ee7fd45920c74203c3c3ea9ad0753d
tstewart <tstewart@klaudhaus.com>
2020-09-18 15:40:03 +1000

    v 1.0.0-beta-11 Improve Typescript definitions

--- index.d.ts
+++ index.d.ts
@@ -4,30 +4,30 @@ type Up = (handler: Update, data?: unknown, options?: UpOptions) => unknown
 
 // Simulate TemplateResult from lit-html as that lib is not a runtime dep
 type TemplateResult = {
-  strings: TemplateStringsArray
-  values: unknown[]
-  type: string
-  getHTML: (string) => string
+  readonly strings: TemplateStringsArray
+  readonly values: readonly unknown[]
+  readonly type: string
+  getHTML: (arg0: string) => string
   getTemplateElement: () => HTMLTemplateElement
 }
 
 type Model = unknown
 type ViewParams = { model: Model, up: Up }
-type View = (ViewParams) => TemplateResult
+type View = (params: ViewParams) => TemplateResult
 
 type RenderParams = {
   result: unknown
   container: Element | DocumentFragment
   options?: unknown
 }
-type Render = (RenderParams) => void
+type Render = (params: RenderParams) => void
 
 type BootstrapParams = {
   model: Model
   up: Up
   url: URL
 }
-type Boostrap = (BootstrapParams) => unknown
+type Boostrap = (params: BootstrapParams) => unknown
 
 type LoggerParams = {
   update: unknown
@@ -38,15 +38,15 @@ type LoggerParams = {
   time: Date
   isChained: boolean
 }
-type Logger = (LoggerParams) => void
+type Logger = (params: LoggerParams) => void
 
 type AppParams = {
-  model: Model
-  view: View
-  render: Render
-  element: Element | DocumentFragment
-  bootstrap: Boostrap
-  logger: Logger
+  model?: Model
+  view?: View
+  render?: Render
+  element?: Element | DocumentFragment
+  bootstrap?: Boostrap
+  logger?: Logger
 }
 
-export function app (AppParams) : void
+export function app (params: AppParams) : void
